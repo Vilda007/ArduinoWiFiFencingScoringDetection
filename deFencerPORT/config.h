af4dba2d6954c72f8dbfcd8939cdbd6fa4ddbe2e
vilem@kuzelovi.cz <vilem@kuzelovi.cz>
2021-03-11 20:04:28 +0100

    Config for the port

---
+++ deFencerPORT/config.h
@@ -0,0 +1,9 @@
+const char NodeName[] PROGMEM = "Fencer1_";      // node (device) name in mesh
+const int lastIP = 10;                           // last number of fixed IP (192.168.4.lastIP)
+const char MeshName[] PROGMEM = "FencerMesh_";   // mesh name
+const char WiFiPassword[] PROGMEM = "dArtagnan"; // mesh password
+
+// Definings
+#define GreenLED D4     // Green LED PIN
+#define RedLED D3       // Red LED PIN
+#define Hit D0          // Hit (score) PIN
